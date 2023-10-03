da42d536ce99e0d42c0ddea5dcfc697c572af311
cjsrussell <cjosephrussell@gmail.com>
2023-10-03 10:57:56 +0100

    Updated survey_3_coding_Scoring

--- funs/beliefcodeFun.R
+++ funs/beliefcodeFun.R
@@ -1,123 +1,139 @@
-funBeliefCode <- function(df){
+
+#this path needs to be same as that used in script this fun is called to
+source("../../funs/scoringFuns.R")  
+
+
+######### This funsction uses funScore and funScore2, the former codes as numeric, the latter as factors.
+
+funBeliefCode <- function(dat) {
   
-  df <- df %>%
-    mutate(
-      
-      #Belief in God (categorical)
-      belief_god_cat = issp_01, 
-      
-      #Belief in God (binary)
-      belief_god_bin = case_when(as.numeric(issp_01) == 5 | as.numeric(issp_01) == 6  ~ 1,
-                                 TRUE ~ 0),
-      # Belief in God (continuous)
-      belief_god_con = belief_01, 
-      
-      # Agnosticism (continuous)
-      agnosticism_cont = agn_01, 
-      
-      # Agnostic Identity 
-      agnosticism_bin  = case_when(as.numeric(issp_02) == 5 ~ 1,
-                                   TRUE ~ 0),
-      # Atheist Membership
-      atheist_mem = am_01, 
-      
-      # Atheist Identity
-      atheist_id = case_when(as.numeric(issp_02) == 4 ~ 1,
-                             TRUE ~ 0), 
-      # Religious Identification
-      relig__id_1 = rid_01, 
-      
-      # Religious Identity, 
-      relig_id_2 = rid_02, 
-      
-      # Religious Attendence
-      relig_attend = rp_01,
-      
-      # Prayer Frequency
-      prayer_freq = rp_02, 
-      
-      # Religious Objects
-      relig_object = rp_03, 
-      
-      # Anti-religiosity
-      anti_relig = ar_01,
-      
-      # Possibility of Knowing (God)
-      god_knowing = belief_02, 
-      
-      # Confidence in Belief
-      belief_confidence = conf_01, 
-      
-      # Apatheism (god)
-      apatheism_god = apth_01,
-      
-      # Apatheism (life purpose)
-      apatheism_purpose = apth_02,
-      
-      # Apatheism (combined)
-      apx1 = as.numeric(apth_01),  #was buggy using as numeric within rowMeans so these are for computing 
-      apx2 = as.numeric(apth_02),
-      
-      apatheism = rowMeans(across(c("apx1", "apx2"))),
-      
-      # Possibility of Truth
-      truth_possibility = mean_01,
-      
-      # Naturalism  - NEEDS TESTING
-      naturalism = case_when(
-        rowSums(across(all_of(snb_cols), ~grepl("Strongly Disagree |Moderately Disagree | Slightly Disagree", .))) > 0 ~ 1, 
-        TRUE ~ 0),
-      
-      # Afterlife Existence
-      afterlife_exist = snb_01,
-      
-      # Afterlife Punishment
-      afterlife_punish= snb_13,
-      
-      # Afterlife Reward
-      afterlife_reward= snb_14,
-      
-      # Reincarnation
-      reincarnation = snb_02,
-      
-      # Astrology
-      astrology = snb_03,
-      
-      # Mystical People
-      msystical_people= snb_04,
-      
-      # Mystical Objects
-      mystical_objects= snb_05,
-      
-      # Good and Evil
-      good_evil = snb_06,
-      
-      # Universal Spirit or Life Force
-      lifeforce = snb_07,
-      
-      # Karma
-      karma = snb_08,
-      
-      # Fate
-      fate= snb_09,
-      
-      # Supernatural Beings Existence
-      supernat_beings = snb_10,
-      
-      # Good Supernatural Beings
-      good_beings = snb_11,
-      
-      # Harmful Supernatural Beings
-      harmful_beings= snb_12,
-      
-      # Evil Eye
-      evil_eye= snb_15,
-      
-      # Personal Spiritual Experience
-      spiritual_force = snb_16,
-      
-      #Lucky Object Carrying
-      lucky_objects = snb_17  )
+# ISSP  
   
-  return(df)
+#here we set the response options (which will be the factor levels)
+  
+  issp_01_levels  <- c(                                                                        
+    "I don't believe in God",
+    "I don't know whether there is a God, and I don't believe there is any way to find out",
+    "I don't believe in a personal God, but I do believe in a Higher Power of some kind",
+    "I find myself believing in God some of the time, but not at others",
+    "While I have doubts, I feel that I do believe in God.",
+    "I know God really exists and I have no doubt about it.",
+    "Don't know")
+  
+  issp_02_levels <- c(
+    "Spiritual but not religious",
+    "Seeker",
+    "Non-religious",
+    "Atheist",
+    "Agnostic",
+    "Humanist",
+    "Sceptic",
+    "Free thinker",
+    "Rationalist",
+    "Secular",
+    "Christian",
+    "Muslim",
+    "Jewish",
+    "Buddhist",
+    "Hindu",
+    "Daoist",
+    "Shinto",
+    "Confucian",
+    "Other (please specify)")
+  
+#here we turn the variable into a factor.
+  dat <- funScore2(dat, "issp_01", responsescale = "custom", customlev = issp_01_levels)  
+  
+  dat <- funScore2(dat, "issp_02", responsescale = "custom", customlev = issp_02_levels)  
+  
+# Religious Identification
+
+# scored numerically with funScore
+ 
+  dat <- funScore(dat, "rid_01", "yesno", FALSE)
+
+  rid_02_levels <- c(
+    "Protestant",
+    "Catholic",
+    "Orthodox (e.g. Greek Orthodox, Russian Orthodox)",
+    "Sunni",
+    "Shiite",
+    "Buddhist",
+    "Confucian",
+    "Daoist",
+    "Hindu",
+    "Jewish",
+    "Shinto",
+    "Other (please specify)")
+  
+  dat <- funScore2(dat, "rid_02", responsescale = "custom", customlev = rid_02_levels)
+  
+  
+# Religious Practice
+  
+  rp_01_levels <- rev(c(    #we reverse this to make the order lowest to highest
+    "More than once a week",
+    "Once a week",
+    "Once a month",
+    "Only on special holy days",
+    "Once a year",
+    "Less often",
+    "Never, practically never"
+  ))
+  
+  rp_02_levels <- rev(c(  #we reverse this to make the order lowest to highest
+    "Several times a day",
+    "Once a day",
+    "Several times each week",
+    "Only when attending religious services",
+    "Only on special holy days",
+    "Once a year",
+    "Less often",
+    "Never, practically never"
+  ))
+  
+  rp_03_levels <- c(
+    "Yes, for religious reasons",
+    "Yes, for non-religious reasons",
+    "No")
+  
+  dat <- funScore2(dat, "rp_01", responsescale = "custom", customlev = rp_01_levels, ordered = TRUE)
+    
+  dat <- funScore2(dat, "rp_02", responsescale = "custom", customlev = rp_02_levels, ordered = TRUE) 
+  
+  dat <- funScore2(dat, "rp_03", responsescale = "custom", customlev = rp_03_levels)
+  
+  
+# Supernatural Belief
+  
+  snb_cols <- colnames(dat %>%
+                           select(starts_with("snb_")) %>%
+                           select(-snb_16, - snb_17)) 
+  snb_16_levels <- c(
+    "Yes, I’ve had an experience like this.",
+    "I’ve had an experience like this, but I didn’t associate it with a spiritual force.",
+    "No, I’ve never had an experience like this.")
+    
+  dat <- funScore2(dat, snb_cols, "agreement", ordered = TRUE)
+  
+  dat <- funScore2(dat, "snb_16", "custom", snb_16_levels)
+  
+  dat <- funScore(dat, "snb_17", "yesno", reverse = FALSE)
+  
+  
+# General Belief
+  
+  gb_cols <- colnames(dat %>%
+                        select(starts_with(c("belief", "conf_", "ar_", "apth_", "agn_", "mean_"))))
+  
+  dat <- funScore2(dat, gb_cols, "agreement", ordered = TRUE)
+  
+  
+# Atheist Membership
+  
+  dat <- funScore(dat, "am_01", "yesno", reverse = FALSE)
+  
+  
+return(dat)
+  
 }
---
+++ funs/beliefscoreFun.R
@@ -0,0 +1,129 @@
+funBeliefScore <- function(df){
+  
+  
+  snb_cols <- colnames(df %>%
+                         select(starts_with("snb_")) %>%
+                         select(-snb_16, - snb_17)) 
+  
+  df <- df %>%
+    mutate(
+      
+      #Belief in God (categorical)
+      belief_god_cat = issp_01, 
+      
+      #Belief in God (binary)
+      belief_god_bin = case_when(as.numeric(issp_01) == 5 | as.numeric(issp_01) == 6  ~ 1,
+                                 TRUE ~ 0),
+      # Belief in God (continuous)
+      belief_god_con = belief_01, 
+      
+      # Agnosticism (continuous)
+      agnosticism_cont = agn_01, 
+      
+      # Agnostic Identity 
+      agnosticism_bin  = case_when(as.numeric(issp_02) == 5 ~ 1,
+                                   TRUE ~ 0),
+      # Atheist Membership
+      atheist_mem = am_01, 
+      
+      # Atheist Identity
+      atheist_id = case_when(as.numeric(issp_02) == 4 ~ 1,
+                             TRUE ~ 0), 
+      # Religious Identification
+      relig__id_1 = rid_01, 
+      
+      # Religious Identity, 
+      relig_id_2 = rid_02, 
+      
+      # Religious Attendence
+      relig_attend = rp_01,
+      
+      # Prayer Frequency
+      prayer_freq = rp_02, 
+      
+      # Religious Objects
+      relig_object = rp_03, 
+      
+      # Anti-religiosity
+      anti_relig = ar_01,
+      
+      # Possibility of Knowing (God)
+      god_knowing = belief_02, 
+      
+      # Confidence in Belief
+      belief_confidence = conf_01, 
+      
+      # Apatheism (god)
+      apatheism_god = apth_01,
+      
+      # Apatheism (life purpose)
+      apatheism_purpose = apth_02,
+      
+      # Apatheism (combined)
+      apx1 = as.numeric(apth_01),  #was buggy using as numeric within rowMeans so these are for computing 
+      apx2 = as.numeric(apth_02),
+      
+      apatheism = rowMeans(across(c("apx1", "apx2"))),
+      
+      # Possibility of Truth
+      truth_possibility = mean_01,
+      
+      # Naturalism  - NEEDS TESTING
+
+      naturalism = case_when(
+        rowSums(across(all_of(snb_cols), ~grepl("Strongly Disagree |Moderately Disagree | Slightly Disagree", .))) > 0 ~ 1, 
+        TRUE ~ 0),
+      
+      # Afterlife Existence
+      afterlife_exist = snb_01,
+      
+      # Afterlife Punishment
+      afterlife_punish= snb_13,
+      
+      # Afterlife Reward
+      afterlife_reward= snb_14,
+      
+      # Reincarnation
+      reincarnation = snb_02,
+      
+      # Astrology
+      astrology = snb_03,
+      
+      # Mystical People
+      msystical_people= snb_04,
+      
+      # Mystical Objects
+      mystical_objects= snb_05,
+      
+      # Good and Evil
+      good_evil = snb_06,
+      
+      # Universal Spirit or Life Force
+      lifeforce = snb_07,
+      
+      # Karma
+      karma = snb_08,
+      
+      # Fate
+      fate= snb_09,
+      
+      # Supernatural Beings Existence
+      supernat_beings = snb_10,
+      
+      # Good Supernatural Beings
+      good_beings = snb_11,
+      
+      # Harmful Supernatural Beings
+      harmful_beings= snb_12,
+      
+      # Evil Eye
+      evil_eye= snb_15,
+      
+      # Personal Spiritual Experience
+      spiritual_force = snb_16,
+      
+      #Lucky Object Carrying
+      lucky_objects = snb_17  )
+  
+  return(df)
+}
